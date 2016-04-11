f293ca63e76958a710d3c2baa3557c773868f563
cypherkaka <chirag.visavadia@gmail.com>
2016-04-11 13:58:33 +0100

    Introduced log4j

--- src/test/java/sorting/BubbleSortTest.java
+++ src/test/java/sorting/BubbleSortTest.java
@@ -1,10 +1,15 @@
 package sorting;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 public class BubbleSortTest {
 
+    private static final Logger logger = LogManager.getLogger(BubbleSortTest.class);
+
     @Test
-    public void test() {
+    public void testBubbleSort() {
+        logger.info("## ## Method: testBubbleSort");
     }
 }

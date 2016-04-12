6bcb5aea05d9b859ab762c70643f68cc307be0c4
cypherkaka <chirag.visavadia@gmail.com>
2016-04-12 14:03:35 +0100

    Refactored log4j to override logfile each time and not create new file

--- src/test/java/sorting/BubbleSortTest.java
+++ src/test/java/sorting/BubbleSortTest.java
@@ -1,15 +1,20 @@
 package sorting;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class BubbleSortTest {
 
-    private static final Logger logger = LogManager.getLogger(BubbleSortTest.class);
+    private static final Logger logger = Logger.getLogger(BubbleSortTest.class);
 
     @Test
     public void testBubbleSort() {
         logger.info("## ## Method: testBubbleSort");
     }
+
+    @Test
+    public void loadTestBubbleSort() {
+        logger.info("## ## Method: loadTestBubbleSort");
+    }
+
 }
